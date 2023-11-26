17a3ee659818ca0fba5f79ea997e9ba49a36ce23
Leslie M <lesliemariemac@gmail.com>
2023-11-26 18:40:38 -0500

    First Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Select Your Elements
+var subTypeElement = document.querySelector("#subscription"); 
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = document.querySelector("basic");
+var subDuration = [1];
+
+//Change Event Listeners
