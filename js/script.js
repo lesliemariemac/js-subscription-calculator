5c7f4ee021f546645c357b076100f1a7faf71a86
Leslie M <lesliemariemac@gmail.com>
2023-11-26 19:15:28 -0500

    Added select elements, change event listener, and function to calculate cost

--- js/script.js
+++ js/script.js
@@ -2,7 +2,32 @@
 var subTypeElement = document.querySelector("#subscription"); 
 var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
-var subType = document.querySelector("basic");
-var subDuration = [1];
+var subType = "basic";
+var subDuration = 1;
 
 //Change Event Listeners
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    updateSubscriptionDiv();
+    });
+    
+
+    subDurationElement.addEventListener("change", function (e) {
+    subDuration = Number(e.target.value);
+    updateSubscriptionDiv();
+});
+
+//Function to Calculate Cost
+var updateSubscriptionDiv = function () {
+    var monthlyCost = 5; //basic plan
+    if (subType === "standard") {
+        monthlyCost = 7;
+      } else if (subType === "premium") {
+        monthlyCost = 10;
+      }
+var total = subDuration * monthlyCost;
+result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+};
+
+//Call Function to Update the DOM
+//updateSubscriptionDiv.addEventListener("change-event");
